e3fa2e7f8a3a2a9669033f3c33bfada2eb26e484
unknown <suraj.sakhare5869@gmail.com>
2024-09-25 19:23:30 +0530

    First Commit

---
+++ PrimeNumbers/Models/Prime.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumbers.Models
+{
+    internal class Prime
+    {
+        public static string CheckPrime(int number)
+        {
+            //special case for prime number
+            if (number == 1) return $"The entered number {number} is not a Prime Number.";
+
+            //check if the given number is prime or not
+            for (int i = 2; i <= Math.Sqrt(number) + 1; i++)
+            {
+                if (number % i == 0)
+                {
+                    return $"The entered number {number} is not a Prime Number.";
+                }
+            }
+            return $"The entered number {number} is a Prime Number.";
+        }
+    }
+}
---
+++ PrimeNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using PrimeNumbers.Models;
+
+namespace PrimeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Prime.CheckPrime(number));
+        }
+    }
+}
